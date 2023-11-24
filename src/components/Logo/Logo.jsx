90fde3c92f4a77e2aabb534577392b970bbcc3b8
rajan <rajansuhagiya1234g@gmail.com>
2023-11-24 11:17:24 +0530

    Fisrt commit

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo = () => {
+  return <img src={require("../../assets/logo.png")} alt="logo" width={67} />;
+};
+
+export default Logo;
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Search.module.css";
+
+const Search = ({ placeholder }) => {
+  return (
+    <div className={styles.wrapper}>
+      <input className={styles.search} placeholder={placeholder} />
+      <button className={styles.searchbutton}>
+        <img
+          src={require("../../assets/Search-icon.png")} alt = "search" width={20}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
